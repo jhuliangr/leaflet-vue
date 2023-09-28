7043e4c3c6a78cff6dc51d7bb35c205077599f15
Jhulian Garcia <jhpiano9731@gmail.com>
2023-09-28 11:48:00 -0400

    FIRST COMMIT

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+
+  <head>
+    <title>leaflet-map-simple</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+    <Map />
+  </body>
+</template>
+
+<script>
+import Map from './components/Map.vue'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.js'
+
+export default {
+  name: 'App',
+  components: {
+    Map
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+}
+
+#map-title {
+  position: relative;
+  margin-top: 10px;
+  margin-left: 50px;
+  float: left;
+  background: white;
+  border: 2px solid rgba(0, 0, 0, 0.2);
+  padding: 6px 8px;
+  font-family: Helvetica;
+  font-weight: bold;
+  font-size: 24px;
+  z-index: 800;
+}
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="map-title">EDIT map title</div>
+  <div id="map"></div>
+</template>
+
+<script>
+import L from 'leaflet'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted(){
+    const map = L.map('map', {
+        center: [21.5, -79], // EDIT coordinates to re-center map
+        zoom: 6,  // EDIT from 1 (zoomed out) to 18 (zoomed in)
+        scrollWheelZoom: false
+      })
+      L.tileLayer(
+        'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+          attribution: '&copy <a href="https://osm.org/copyright">\
+            OpenStreetMap</a> contributors, &copy\
+            <a href="https://carto.com/attribution">CARTO</a>'
+      }).addTo(map)
+      
+      /* Display a point marker with pop-up text */
+      L.marker([41.77, -72.69]).addTo(map) // EDIT marker coordinates
+      .bindPopup("Insert pop-up text here") // EDIT pop-up text message
+    
+  }
+}
+
+  /* display basemap tiles -- see others at https://leaflet-extras.github.io/leaflet-providers/preview/ */
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
